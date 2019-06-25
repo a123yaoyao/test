e5b2712a0b4422a5c70f8646666ea667b5a127b0
a123yaoyao <291778952@qq.com>
2019-06-25 22:16:07 +0800

    数据同步优化

--- src/main/resources/tb.sql
+++ src/main/resources/tb.sql
@@ -17,3 +17,22 @@ tablespace TIEAF_CUS
     minextents 1
     maxextents unlimited
   );
+
+  create table EAF_ORG_RECORD
+(
+  eaf_id        VARCHAR2(200) not null,
+  eaf_db_name   VARCHAR2(200),
+  eaf_name      VARCHAR2(200),--部门名称
+  BIM_NUM VARCHAR2(200) --部门编号
+)
+tablespace TIEAF_CUS
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 1M
+    minextents 1
+    maxextents unlimited
+  );
